0d317bd4e81b420d20e8ccbaf1783a666b3a9a57
Hasin Mahir <130781004+HasinMahir77@users.noreply.github.com>
2025-03-22 00:07:51 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -58,4 +58,4 @@ def delete_guitar(tag_id):
     return jsonify({"message": "Guitar deleted successfully!"})
 
 if __name__ == '__main__':
-    app.run(port=2000, host="0.0.0.0")
+    app.run(port=3000, host="0.0.0.0")
