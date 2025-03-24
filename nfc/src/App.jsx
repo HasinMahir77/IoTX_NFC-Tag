e59d954d2f082e1f328c52dcaad87253cfe9ec5c
Hasin Mahir <130781004+HasinMahir77@users.noreply.github.com>
2025-03-25 04:49:16 +0600

    Update App.jsx

--- nfc/src/App.jsx
+++ nfc/src/App.jsx
@@ -57,7 +57,7 @@ const MainApp = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/nfc_tag" element={<App server="http://192.168.0.137:3000" />} />
+      <Route path="/nfc_tag" element={<App server="nfc.iotexperience.com:3000" />} />
       <Route path="*" element={<Navigate to="/" />} /> {/* Catch-all route */}
     </Routes>
   </Router>
