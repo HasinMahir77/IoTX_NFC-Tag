d010a010d5125fa317896dff6d5e5e61cd79911d
Hasin Mahir <130781004+HasinMahir77@users.noreply.github.com>
2025-03-25 01:31:29 +0600

    Revert "Update App.jsx"
    This reverts commit d0b4712501762f7db19903c5684dfff3d38143f9.

--- nfc/src/App.jsx
+++ nfc/src/App.jsx
@@ -57,7 +57,7 @@ const MainApp = () => (
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/nfc_tag" element={<App server="199.250.210.176:3000" />} />
+      <Route path="/nfc_tag" element={<App server="nfc.iotexperience.com:3000" />} />
       <Route path="*" element={<Navigate to="/" />} /> {/* Catch-all route */}
     </Routes>
   </Router>
